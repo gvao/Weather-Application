df6024bd336a43105556a485ca100d023164b937
Yuri Galvão <90591430+gvao@users.noreply.github.com>
2023-04-22 10:21:18 -0300

    Criado funcao  getCityWeather

--- js/weather.js
+++ js/weather.js
@@ -10,11 +10,28 @@ const getCityData = async cityName => {
         if (!response.ok) throw new Error('Não foi possível obter os dados')
 
         const [ cityData ] = await response.json()
-        
         return cityData
     } catch ({ name, message }) {
         alert(`${name}: ${message}`)
     }
 }
 
-getCityData('fortaleza')
+const getCityWeather = async cityName => {
+    try {
+        const { Key } = await getCityData(cityName)
+        const cityWeatherUrl = `http://dataservice.accuweather.com/currentconditions/v1/${Key}?apikey=${apiKey}`
+        const response = await fetch(cityWeatherUrl)
+
+        if (!response.ok) throw new Error('Não foi possível obter os dados')
+
+        const [ cityWeatherData ] = await response.json()
+        debugger
+        return cityWeatherData
+        
+    } catch ({ name, message }) {
+        alert(`${name}: ${message}`)
+    }
+}
+// 
+
+getCityWeather('fortaleza')
