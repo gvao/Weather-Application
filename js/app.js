dd15e0aa265f78687c0d62c3989582fb2d6cbf81
Yuri Galvão <90591430+gvao@users.noreply.github.com>
2023-04-22 17:35:24 -0300

    Refactored app.js

--- js/app.js
+++ js/app.js
@@ -3,31 +3,45 @@ const cityForm = document.querySelector('[data-js="change-location"]')
 const cityNameContainer = document.querySelector('[data-js="city-name"]')
 const cityWeatherContainer = document.querySelector('[data-js="city-weather"]')
 const cityTemperatureContainer = document.querySelector('[data-js="city-temperature"]')
+
 const cityCard = document.querySelector('[data-js="city-card"]')
 let timeImg = document.querySelector('[data-js="time"]')
 const timeIconContainer = document.querySelector('[data-js="time-icon"]')
 
+const showCityCard = () => {
+    if (!cityCard.classList.contains('d-none')) return
+    cityCard.classList.remove('d-none')
+}
+
+const insertTimeIcon = ({ WeatherIcon, WeatherText }) => {
+    const timeIcon = `<img src="src/icons/${WeatherIcon}.svg" alt="${WeatherText}" />`
+    timeIconContainer.innerHTML = timeIcon
+}
+
+const insertIcons = ({ WeatherIcon, WeatherText, IsDayTime }) => {
+    timeImg.src = `src/${IsDayTime ? 'day' : 'night'}.svg`
+    
+    insertTimeIcon({ WeatherIcon, WeatherText })
+}
+
+const updateContents = ( LocalizedName, WeatherText, Temperature ) => {
+    cityNameContainer.textContent = LocalizedName
+    cityWeatherContainer.textContent = WeatherText
+    cityTemperatureContainer.textContent = Temperature
+}
+
 cityForm.addEventListener('submit', async event => {
+    const { target } = event
     event.preventDefault()
 
-    const inputValue = event.target.city.value
+    const inputValue = target.city.value
 
     const [{ Key, LocalizedName }] = await getCityData(inputValue)
     const [{ WeatherText, Temperature, IsDayTime, WeatherIcon }] = await getCityWeather(Key)
 
-    const timeIcon = `<img src="src/icons/${WeatherIcon}.svg" alt="${WeatherText}" />`
+    showCityCard()
+    insertIcons({ WeatherIcon, WeatherText, IsDayTime })
+    updateContents(LocalizedName, WeatherText, Temperature.Metric.Value)
 
-    if (cityCard.classList.contains('d-none')){
-        cityCard.classList.remove('d-none')
-    }
-
-    if (IsDayTime) timeImg.src = 'src/day.svg'
-    else timeImg.src = 'src/night.svg' 
-
-    timeIconContainer.innerHTML = timeIcon
-    cityNameContainer.textContent = LocalizedName
-    cityWeatherContainer.textContent = WeatherText
-    cityTemperatureContainer.textContent = Temperature.Metric.Value
-
-    cityForm.reset()
+    target.reset()
 })
