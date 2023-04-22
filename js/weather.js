86d885f0809bce95c212c505ca1dd1f3b10761b1
Yuri Galvão <90591430+gvao@users.noreply.github.com>
2023-04-22 10:06:03 -0300

    Criado funcao para api/city-search

--- js/weather.js
+++ js/weather.js
@@ -1,1 +1,20 @@
 const apiKey = 'VtC3Y3cqRsVUBdACrBQQFb5NIWgz5H7m'
+
+const getCityUrl = cityName =>  `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${apiKey}&q=${cityName}`
+
+const getCityData = async cityName => {
+    try {
+        const cityUrl = getCityUrl(cityName)
+        const response = await fetch(cityUrl)
+
+        if (!response.ok) throw new Error('Não foi possível obter os dados')
+
+        const [ cityData ] = await response.json()[0]
+        
+        return cityData
+    } catch ({ name, message }) {
+        alert(`${name}: ${message}`)
+    }
+}
+
+getCityData('fortaleza')
