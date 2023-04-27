805c2dd5618d25e9bb90bc53efa31891fa7caff2
Yuri Galvão <90591430+gvao@users.noreply.github.com>
2023-04-27 18:18:41 -0300

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -1,5 +1,5 @@
 const apiKey = '3877vB4OwT5MrAbvOZU5D5avrwe9U0AV'
-const urlBase = 'http://dataservice.accuweather.com/'
+const urlBase = 'https://dataservice.accuweather.com/'
 
 const getUrlSearchCity = query => `${urlBase}locations/v1/cities/search?language=pt-BR&apikey=${apiKey}&q=${query}`
 const getUrlWeather = cityCod => `${urlBase}currentconditions/v1/${cityCod}?language=pt-BR&apikey=${apiKey}`
