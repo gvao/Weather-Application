ce5df39b42b4841e9fa38004a3eeb0b1b9fe6c72
Yuri Galvão <90591430+gvao@users.noreply.github.com>
2023-04-27 16:49:08 -0300

    Create form

--- js/app.js
+++ js/app.js
@@ -1,49 +1,15 @@
-const cityForm = document.querySelector('[data-js="change-location"]')
+const $formCitySearch = document.querySelector('.search-form')
+const $img = document.querySelector('.card-weather img')
+const $icon = document.querySelector( '[data-js="icon-weather"]')
 
-const cityNameContainer = document.querySelector('[data-js="city-name"]')
-const cityWeatherContainer = document.querySelector('[data-js="city-weather"]')
-const cityTemperatureContainer = document.querySelector('[data-js="city-temperature"]')
-
-const cityCard = document.querySelector('[data-js="city-card"]')
-let timeImg = document.querySelector('[data-js="time"]')
-const timeIconContainer = document.querySelector('[data-js="time-icon"]')
-
-const showCityCard = () => {
-    if (!cityCard.classList.contains('d-none')) return
-    cityCard.classList.remove('d-none')
-}
-
-const insertIcons = ({ WeatherIcon, WeatherText, IsDayTime }) => {
-    timeImg.src = `src/${IsDayTime ? 'day' : 'night'}.svg`
-
-    const timeIcon = `<img src="src/icons/${WeatherIcon}.svg" alt="${WeatherText}" />`
-    timeIconContainer.innerHTML = timeIcon
-}
-
-const updateContents = ( LocalizedName, WeatherText, Temperature ) => {
-    cityNameContainer.textContent = LocalizedName
-    cityWeatherContainer.textContent = WeatherText
-    cityTemperatureContainer.textContent = Temperature
-}
-
-const showCityWeatherInfo = async inputValue => {
-    const [{ Key, LocalizedName }] = await getCityData(inputValue)
-    // const [ weatherData ] = await getCityWeather(Key)
-    const [{ WeatherText, WeatherIcon, Temperature, IsDayTime }] = await getCityWeather(Key)
-    
-    insertIcons({ WeatherIcon, WeatherText, IsDayTime })
-    updateContents(LocalizedName, WeatherText, Temperature.Metric.Value)
-}
-
-const handlerSubmit = event => {
+$formCitySearch.addEventListener('submit', async event => {
     event.preventDefault()
     const { target } = event
+    const inputValue = target.citysearch.value
 
-    const inputValue = target.city.value
-    
-    showCityCard()
-    showCityWeatherInfo(inputValue)
-    target.reset()
-}
+    const city = await getCity(inputValue)
+    const { IsDayTime, Temperature, WeatherIcon, WeatherText } = await getCityWeather(city.Key)
 
-cityForm.addEventListener('submit', handlerSubmit)
+    $img.src = `src/${ IsDayTime ? 'day' : 'night' }.svg`
+    $img.alt = WeatherText
+})
