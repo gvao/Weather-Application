8ac8638b601ba3da2c6a5e756a104cc3af5f1e0f
Yuri Galvão <90591430+gvao@users.noreply.github.com>
2023-04-27 17:32:52 -0300

    Refactoring

--- js/app.js
+++ js/app.js
@@ -1,19 +1,16 @@
 const $formCitySearch = document.querySelector('.search-form')
-const $img = document.querySelector('.card-weather img')
+
+const $card = document.querySelector('.card-weather')
+const $img = document.querySelector('[data-js="icon-weather"]')
 const $icon = document.querySelector('[data-js="icon-weather"]')
 const $cityName = document.querySelector('[data-js="city-name"]')
 const $cityWeather = document.querySelector('[data-js="city-weather"]')
 const $cityTemperature = document.querySelector('[data-js="city-temperature"] > span')
 
-$formCitySearch.addEventListener('submit', async event => {
-    event.preventDefault()
-    const { target } = event
-    const inputValue = target.citysearch.value
+const updateScreen = ({ IsDayTime, WeatherIcon, WeatherText, LocalizedName, Temperature }) => {
 
-    const { LocalizedName, Key } = await getCity(inputValue)
-    const {
-        IsDayTime, Temperature, WeatherIcon, WeatherText
-    } = await getCityWeather(Key)
+    const cardClasses = $card.classList
+    cardClasses.contains('hidden') && cardClasses.remove('hidden')
 
     $img.src = `src/${IsDayTime ? 'day' : 'night'}.svg`
     $img.alt = WeatherText
@@ -22,5 +19,27 @@ $formCitySearch.addEventListener('submit', async event => {
 
     $cityWeather.textContent = WeatherText
     $cityName.textContent = LocalizedName
-    $cityTemperature.textContent = Temperature.Metric.Value
+    $cityTemperature.textContent = Temperature
+}
+
+const getRequestApi = async inputValue => {
+    const { LocalizedName, Key } = await getCity(inputValue)
+    const {
+        IsDayTime, Temperature, WeatherIcon, WeatherText
+    } = await getCityWeather(Key)
+
+    return {
+        IsDayTime, WeatherIcon, WeatherText, LocalizedName, Temperature: Temperature.Metric.Value
+    }
+}
+
+$formCitySearch.addEventListener('submit', event => {
+    event.preventDefault()
+    const { target } = event
+    const inputValue = target.citysearch.value
+
+    getRequestApi(inputValue)
+        .then(updateScreen)
+        .catch(console.error)
+        .finally(() => target.reset())
 })
